23abbacee6105d67c8b2e63ce1385545a3e9ef67
unknown <anjalikesavarapu@gmail.com>
2022-06-18 21:16:31 +0530

    Generated dice values using random function by settimg state

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import React from 'react';
 import './App.css';
 import Die from './components/Die';
 
+
+
 function App() {
+
+  function allNewDice() {
+    const newDice = []
+    for (var i = 0; i < 10; i++) {
+      newDice.push(Math.floor(Math.random() * 6) + 1)
+    }
+    return newDice
+  }
+
+  const [dice, setDice] = React.useState(allNewDice())
+
   return (
     <main>
       <div className='dice--container'>
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={6} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
+        {dice.map((die) => <Die value={die} />)}
       </div>
     </main>
   );
