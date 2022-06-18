0a581a5cc3540eb5b2d045e799de53f24e3809a7
unknown <anjalikesavarapu@gmail.com>
2022-06-18 20:45:06 +0530

    creating and styling Die component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
+import Die from './components/Die';
 
 function App() {
   return (
     <main>
-
+      <div className='dice--container'>
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={6} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+      </div>
     </main>
   );
 }
---
+++ src/components/Die.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Die(props) {
+    return (
+        <div>
+            <h1 className="die">{props.value}</h1>
+        </div>
+
+    )
+}
